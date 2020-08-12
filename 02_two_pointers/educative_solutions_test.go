c446e08e80a107e80bce695dce8ff8e6aaed41a4
John <1340762527@qq.com>
2020-08-12 22:26:17 +0800

    add code

--- 02_two_pointers/educative_solutions_test.go
+++ 02_two_pointers/educative_solutions_test.go
@@ -105,8 +105,7 @@ func TestSquaringSortedArray(t *testing.T) {
 // TripletSumToZero
 // 同一个题见leetcode https://leetcode-cn.com/problems/3sum/submissions/
 // 再次做时间从 375ms 提高到了 40ms，而且代码量也减少了
-// 本题关键是如何做到准确的去重复
-// 还有就是i，j是可以同时增减的（按照题目的要求）
+// 本题关键是如何精准的去重
 func TripletSumToZero(arr []int) [][]int {
 	result := [][]int{}
 
@@ -121,20 +120,21 @@ func TripletSumToZero(arr []int) [][]int {
 		i := cur + 1
 		j := len(arr) - 1
 		for i < j {
-			if arr[i]+arr[j] == -a {
+			if arr[i]+arr[j] < -a {
+				i++
+				for i < j && arr[i] == arr[i-1] {
+					i++
+				}
+			} else if arr[i]+arr[j] > -a {
+				j--
+				for i < j && arr[j] == arr[j+1] {
+					j--
+				}
+			} else {
 				result = append(result, []int{a, arr[i], arr[j]})
 				i++
 				for i < j && arr[i] == arr[i-1] {
 					i++
-				}
-			} else {
-				i++
-				for i < j && arr[i] == arr[i-1] {
-					i++
-				}
-				j--
-				for i < j && arr[j] == arr[j+1] {
-					j--
 				}
 			}
 		}
