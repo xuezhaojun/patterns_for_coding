b14824f973e33abecb8b81a0478c71997037aa97
John <1340762527@qq.com>
2020-09-07 11:48:25 +0800

    add code

--- 02_two_pointers/leetcode_test.go
+++ 02_two_pointers/leetcode_test.go
@@ -46,3 +46,39 @@ func threeSumClosest(nums []int, target int) int {
 func TestThreeSumClosest(t *testing.T) {
 	t.Log(threeSumClosest([]int{0, 2, 1, -3}, 1))
 }
+
+// 长键按入
+// https://leetcode-cn.com/problems/long-pressed-name/solution/
+// 通过使用count的方式来解题，我也想到了，但是最终没有引入，反而导致思路越来越乱
+// 有时候引入变量可以让code变复杂，但是思路变清晰，这时候要果断的引入变量【重要】
+// 引入count的运算，让算法非常的简洁漂亮
+func isLongPressedName(name string, typed string) bool {
+	nlen := len(name)
+	tlen := len(typed)
+	i := 1
+	j := 1
+	counti := 0
+	countj := 0
+	for i < nlen || j < tlen {
+		for i < nlen && name[i] == name[i-1] {
+			i++
+			counti++
+		}
+		for j < tlen && typed[j] == typed[j-1] {
+			j++
+			countj++
+		}
+		if typed[j-1] != name[i-1] || countj < counti {
+			return false
+		}
+		i++
+		j++
+		counti, countj = 0, 0
+	}
+	return name[nlen-1] == typed[tlen-1]
+}
+
+func TestIsLongPressedName(t *testing.T) {
+	t.Log(isLongPressedName("leelee", "lleeelee"))
+	t.Log(isLongPressedName("kikcxmvzi", "kiikcxxmmvvzz"))
+}
