af011be2c7be29ec1a61680c006649adab5b5c4a
John <1340762527@qq.com>
2020-09-10 05:49:29 +0800

    add code

--- 06_in_place_reversal_of_a_linked_list/educative_test.go
+++ 06_in_place_reversal_of_a_linked_list/educative_test.go
@@ -1,5 +1,7 @@
 package in_place_reversal_of_a_linked_list
 
+import "testing"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
@@ -72,6 +74,7 @@ func reverseBetween(head *ListNode, m int, n int) *ListNode {
 
 // Reverse every K-element Sub-list
 // https://leetcode-cn.com/problems/reverse-nodes-in-k-group/
+// 如果本题进行考察的话，即属于考察人员的复杂问题的快速解决能力，目前这部分我还有欠缺，比如下列代码还不够简洁，同时解题速度也不够快
 func reverseKGroup(head *ListNode, k int) *ListNode {
 	preTail := &ListNode{}
 	cur := head
@@ -84,26 +87,30 @@ func reverseKGroup(head *ListNode, k int) *ListNode {
 		// 找到对应的newHead,和newTail
 		oldHead = cur
 		var pre *ListNode
-		for cur != nil {
+		for cur != nil && count > 0 {
 			pre = cur
 			cur = cur.Next
 			count--
 		}
 		oldTail = pre
 
-		if count > 0 {
+		if count == 0 {
 			// 进行从 oldHead 到 oldTail 的反转
-			c := oldTail
+			c := oldHead
 			var p *ListNode
-			for c != oldTail.Next {
+			for count < k {
 				next := c.Next
 				c.Next = p
 				p = c
 				c = next
+				count++
 			}
+			newHead = oldTail
+			newTail = oldHead
+		} else {
+			newHead = oldHead
+			newTail = oldTail
 		}
-		newHead = oldHead
-		newTail = oldTail
 
 		// 拼接
 		preTail.Next = newHead
@@ -116,4 +123,28 @@ func reverseKGroup(head *ListNode, k int) *ListNode {
 		}
 	}
 	return head
+}
+
+func TestReverseKGroup(t *testing.T) {
+	n5 := &ListNode{
+		Val:  5,
+		Next: nil,
+	}
+	n4 := &ListNode{
+		Val:  4,
+		Next: n5,
+	}
+	n3 := &ListNode{
+		Val:  3,
+		Next: n4,
+	}
+	n2 := &ListNode{
+		Val:  2,
+		Next: n3,
+	}
+	n1 := &ListNode{
+		Val:  1,
+		Next: n2,
+	}
+	reverseKGroup(n1, 3)
 }
