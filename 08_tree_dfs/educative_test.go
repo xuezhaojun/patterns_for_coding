6c48415b86fe3b3e2ec2dda3da17a6a248a9bcaf
xuezhaojun <1340762527@qq.com>
2021-01-01 16:16:35 +0800

    add code

--- 08_tree_dfs/educative_test.go
+++ 08_tree_dfs/educative_test.go
@@ -26,6 +26,8 @@ func hasPathSum(root *TreeNode, sum int) bool {
 
 // All Paths for a Sum
 // https://leetcode-cn.com/problems/path-sum-ii/
+// 空间使用有待优化，毕竟每次传递[][]int的数据是不小的开销
+// 使用同时传递 current path 和 all path的思路
 func pathSum(root *TreeNode, sum int) [][]int {
 	if root == nil {
 		return [][]int{}
@@ -54,4 +56,115 @@ func pathSum(root *TreeNode, sum int) [][]int {
 	}
 
 	return result
+}
+
+// Sum of Path Numbers
+// https://leetcode-cn.com/problems/sum-root-to-leaf-numbers/?utm_source=LCUS&utm_medium=ip_redirect&utm_campaign=transfer2china
+// 其实本题就是上面题的变式，首先还是需要求出所有的path，然后将所有的path变成数字加起来即可
+// 本题学习的是dfs从上到下的思路，即可以从低而上，也可以从顶而下【重要】
+func sumNumbers(root *TreeNode) int {
+	return dfs(root, 0)
+}
+
+func dfs(root *TreeNode, preSum int) int {
+	if root == nil {
+		return 0
+	}
+
+	sum := preSum*10 + root.Val
+
+	// if node is leaf
+	if root.Left == nil && root.Right == nil {
+		return sum
+	}
+
+	return dfs(root.Left, sum) + dfs(root.Right, sum)
+}
+
+// Path With Given Sequence
+func pathWithSequence(root *TreeNode, seq []int) bool {
+	if root == nil {
+		return false
+	}
+
+	if len(seq) < 1 {
+		return false
+	}
+
+	if root.Val != seq[0] {
+		return false
+	}
+
+	if len(seq) == 1 && root.Left == nil && root.Right == nil && root.Val == seq[0] {
+		return true
+	}
+
+	return pathWithSequence(root.Left, seq[1:]) || pathWithSequence(root.Right, seq[1:])
+}
+
+// Count Paths for a Sum
+// https://leetcode-cn.com/problems/path-sum-iii/
+// 本题的巧妙在于，每次统计当前path下满足的数量时候，都是必须加上当前节点，确保了不重复序列的【不重复】
+func pathSum3(root *TreeNode, sum int) int {
+	return dfs_3(root, []int{}, sum)
+}
+
+func dfs_3(root *TreeNode, path []int, sum int) int {
+	if root == nil {
+		return 0
+	}
+
+	same := 0
+	path = append(path, root.Val)
+	temp := 0
+	for i := len(path) - 1; i >= 0; i-- {
+		temp += path[i]
+		if temp == sum {
+			same += 1
+		}
+	}
+
+	return same + dfs_3(root.Left, path, sum) + dfs_3(root.Right, path, sum)
+}
+
+// Tree Diameter
+// https://leetcode.com/problems/diameter-of-binary-tree/
+func diameterOfBinaryTree(root *TreeNode) int {
+	var ans = 1       // 本题中，定义了ans，一个全局变量
+	depth(root, &ans) // 此处传递的了ans的地址，而不是ans本身
+	return ans - 1
+}
+
+func depth(root *TreeNode, ans *int) int {
+	if root == nil {
+		return 0
+	}
+
+	left := depth(root.Left, ans)
+	right := depth(root.Right, ans)
+
+	// depth中需要更新两个概念：
+	// length是我们最终需要返回的答案，是两个leaf之间的距离
+	// 而depth则是当前node的深度
+	// 通过这两个概念可以简化左右节点同时记录的麻烦
+
+	newLength := left + right + 1
+	if *ans < newLength {
+		*ans = newLength
+	}
+
+	var newDepth int
+	if left > right {
+		newDepth = left
+	} else {
+		newDepth = right
+	}
+
+	return newDepth + 1
+}
+
+// Path with Maximum Sum
+// https://leetcode-cn.com/problems/binary-tree-maximum-path-sum/
+func maxPathSum(root *TreeNode) int {
+
 }
